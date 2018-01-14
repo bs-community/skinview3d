a654cd20e1ad5d65e442d628e5c00cc7c06bc9f9
Pig Fang <g-plane@hotmail.com>
2018-01-14 20:46:20 +0800

    fix import three.js

--- src/viewer.js
+++ src/viewer.js
@@ -1,4 +1,4 @@
-import THREE from "three";
+import * as THREE from "three";
 import { PlayerObject } from "./model";
 import { OrbitControls } from "./orbit_controls";
 import { invokeAnimation } from "./animation";
