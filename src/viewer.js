670647109a8aeb69ffa992df900a5f51cc23e76c
yushijinhun <yushijinhun@gmail.com>
2018-07-02 19:58:42 +0800

    Polish

--- src/viewer.js
+++ src/viewer.js
@@ -51,22 +51,22 @@ function computeSkinScale(width) {
 	return width / 64.0;
 }
 
-function convertSkinTo1_8(context, width) {
+function convertSkinTo1_8(skinContext, width) {
 	let scale = computeSkinScale(width);
-	let copySkin = (context, sX, sY, w, h, dX, dY, flipHorizontal) => copyImage(context, sX * scale, sY * scale, w * scale, h * scale, dX * scale, dY * scale, flipHorizontal);
+	let copySkin = (sX, sY, w, h, dX, dY, flipHorizontal) => copyImage(skinContext, sX * scale, sY * scale, w * scale, h * scale, dX * scale, dY * scale, flipHorizontal);
 
-	copySkin(context, 4, 16, 4, 4, 20, 48, true); // Top Leg
-	copySkin(context, 8, 16, 4, 4, 24, 48, true); // Bottom Leg
-	copySkin(context, 0, 20, 4, 12, 24, 52, true); // Outer Leg
-	copySkin(context, 4, 20, 4, 12, 20, 52, true); // Front Leg
-	copySkin(context, 8, 20, 4, 12, 16, 52, true); // Inner Leg
-	copySkin(context, 12, 20, 4, 12, 28, 52, true); // Back Leg
-	copySkin(context, 44, 16, 4, 4, 36, 48, true); // Top Arm
-	copySkin(context, 48, 16, 4, 4, 40, 48, true); // Bottom Arm
-	copySkin(context, 40, 20, 4, 12, 40, 52, true); // Outer Arm
-	copySkin(context, 44, 20, 4, 12, 36, 52, true); // Front Arm
-	copySkin(context, 48, 20, 4, 12, 32, 52, true); // Inner Arm
-	copySkin(context, 52, 20, 4, 12, 44, 52, true); // Back Arm
+	copySkin(4, 16, 4, 4, 20, 48, true); // Top Leg
+	copySkin(8, 16, 4, 4, 24, 48, true); // Bottom Leg
+	copySkin(0, 20, 4, 12, 24, 52, true); // Outer Leg
+	copySkin(4, 20, 4, 12, 20, 52, true); // Front Leg
+	copySkin(8, 20, 4, 12, 16, 52, true); // Inner Leg
+	copySkin(12, 20, 4, 12, 28, 52, true); // Back Leg
+	copySkin(44, 16, 4, 4, 36, 48, true); // Top Arm
+	copySkin(48, 16, 4, 4, 40, 48, true); // Bottom Arm
+	copySkin(40, 20, 4, 12, 40, 52, true); // Outer Arm
+	copySkin(44, 20, 4, 12, 36, 52, true); // Front Arm
+	copySkin(48, 20, 4, 12, 32, 52, true); // Inner Arm
+	copySkin(52, 20, 4, 12, 44, 52, true); // Back Arm
 }
 
 function isSlimSkin(skinContext, width) {
