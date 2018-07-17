e6be73fc9dabf873078f0ca9b44ef71d6ac8b091
Sean Boult <hacksore@mcskinsearch.com>
2018-07-17 13:58:27 -0500

    Add back in the main entry point content

---
+++ src/skinview3d.ts
@@ -0,0 +1,26 @@
+export {
+	SkinObject,
+	CapeObject,
+	PlayerObject
+} from "./model";
+
+export {
+	SkinViewer
+} from "./viewer";
+
+export {
+	OrbitControls,
+	createOrbitControls
+} from "./orbit_controls";
+
+export {
+	invokeAnimation,
+	CompositeAnimation,
+	WalkingAnimation,
+	RunningAnimation,
+	RotatingAnimation
+} from "./animation";
+
+export {
+	isSlimSkin
+} from "./utils";
