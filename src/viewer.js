fc0e12587e0d8190c79c5826a50c478de14424e6
yushijinhun <yushijinhun@gmail.com>
2018-08-16 19:35:16 +0800

    Fix options.detectModel not working
    Cherry-Pick: 0545117c51382fa3cf8dde0bd45c42563343a04c

--- src/viewer.js
+++ src/viewer.js
@@ -7,7 +7,7 @@ class SkinViewer {
 	constructor(options) {
 		this.domElement = options.domElement;
 		this.animation = options.animation || null;
-		this.detectModel = options.animation !== false; // true by default
+		this.detectModel = options.detectModel !== false; // true by default
 		this.animationPaused = false;
 		this.animationTime = 0;
 		this.disposed = false;
