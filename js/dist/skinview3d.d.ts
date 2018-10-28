bea60c3d85e3b3f5c6067cc38fc1f9b8d23667f2
Hacksore <hacksore@mcskinsearch.com>
2018-10-28 10:55:30 -0500

    Update demo
    Change CSS to make a bit better from UX perspective
    Add layer toggle for all the parts
    Change skin/cape to dropdown
    Fix the path for output from build->dist in update script
    Add HD cape

---
+++ js/dist/skinview3d.d.ts
@@ -0,0 +1,5 @@
+export { SkinObject, BodyPart, CapeObject, PlayerObject } from "./model";
+export { SkinViewer, SkinViewerOptions } from "./viewer";
+export { OrbitControls, createOrbitControls } from "./orbit_controls";
+export { IAnimation, AnimationFn, Animation, invokeAnimation, AnimationHandle, CompositeAnimation, WalkingAnimation, RunningAnimation, RotatingAnimation } from "./animation";
+export { isSlimSkin } from "./utils";
---
+++ js/dist/utils.d.ts
@@ -0,0 +1,3 @@
+export declare function loadSkinToCanvas(canvas: HTMLCanvasElement, image: HTMLImageElement): void;
+export declare function loadCapeToCanvas(canvas: HTMLCanvasElement, image: HTMLImageElement): void;
+export declare function isSlimSkin(canvasOrImage: HTMLCanvasElement | HTMLImageElement): boolean;
