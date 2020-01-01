cb1f00dd177ecb8d620b7b65d65738a6694906b1
yushijinhun <yushijinhun@gmail.com>
2020-01-01 15:55:17 +0800

    Remove access to renderer context

--- src/viewer.ts
+++ src/viewer.ts
@@ -76,7 +76,6 @@ export class SkinViewer {
 
 		this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: false });
 		this.renderer.setSize(300, 300); // default size
-		this.renderer.context.getShaderInfoLog = () => ""; // shut firefox up
 		this.domElement.appendChild(this.renderer.domElement);
 
 		this.playerObject = new PlayerObject(this.layer1Material, this.layer2Material, this.capeMaterial);
