5fd72eefcf132fd420574710745fb294891c2c5e
yushijinhun <yushijinhun@gmail.com>
2020-01-23 00:19:47 +0800

    use export *

--- src/skinview3d.ts
+++ src/skinview3d.ts
@@ -1,32 +1,4 @@
-export {
-	SkinObject,
-	BodyPart,
-	CapeObject,
-	PlayerObject
-} from "./model";
-
-export {
-	SkinViewer,
-	SkinViewerOptions
-} from "./viewer";
-
-export {
-	OrbitControls,
-	createOrbitControls
-} from "./orbit_controls";
-
-export {
-	IAnimation,
-	AnimationFn,
-	Animation,
-	invokeAnimation,
-
-	SubAnimationHandle,
-	AnimationHandle,
-	CompositeAnimation,
-	RootAnimation,
-
-	WalkingAnimation,
-	RunningAnimation,
-	RotatingAnimation
-} from "./animation";
+export * from "./model";
+export * from "./viewer";
+export * from "./orbit_controls";
+export * from "./animation";
