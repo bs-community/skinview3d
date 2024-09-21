4259230c825f2dd2b2ad2113e6cf48eb84c67c0e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-21 10:58:21 -0500

    This formatings wrongly with the cli?

--- src/viewer.ts
+++ src/viewer.ts
@@ -162,11 +162,11 @@ export interface SkinViewerOptions {
 	 * @defaultValue If unspecified, the ears will be invisible.
 	 */
 	ears?:
-		| "current-skin"
-		| {
-				textureType: "standalone" | "skin";
-				source: RemoteImage | TextureSource;
-		  };
+	| "current-skin"
+	| {
+		textureType: "standalone" | "skin";
+		source: RemoteImage | TextureSource;
+	};
 
 	/**
 	 * Whether to preserve the buffers until manually cleared or overwritten.
