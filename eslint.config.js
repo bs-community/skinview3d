ae135a30375d2c8b9299df477ab1a3ddaaefd165
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-04 09:31:11 -0500

    Cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,30 @@
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsdocPlugin from 'eslint-plugin-tsdoc';
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsdocPlugin from "eslint-plugin-tsdoc";
 
 export default [
 	{
-		ignores: ['bundles/**/*', 'libs/**/*']
+		ignores: ["dist/**/*"],
 	},
 	{
-		files: ['**/*.ts'],
+		files: ["**/*.ts"],
 		languageOptions: {
 			parser: tsParser,
 			parserOptions: {
-				ecmaVersion: 'latest',
-				sourceType: 'module'
-			}
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
 		},
 		plugins: {
-			'@typescript-eslint': tsPlugin,
-			'tsdoc': tsdocPlugin
+			"@typescript-eslint": tsPlugin,
+			tsdoc: tsdocPlugin,
 		},
 		rules: {
-			'indent': ['error', 'tab'],
-			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-			'@typescript-eslint/no-inferrable-types': 'off',
-			'@typescript-eslint/no-empty-interface': 'off',
-			'tsdoc/syntax': 'warn'
-		}
-	}
-]; 
+			indent: ["error", "tab"],
+			"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+			"@typescript-eslint/no-inferrable-types": "off",
+			"@typescript-eslint/no-empty-interface": "off",
+			"tsdoc/syntax": "warn",
+		},
+	},
+];
